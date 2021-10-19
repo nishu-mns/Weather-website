3534df769a62440e744c0e3b27c21b7f025ea795
nishu-mns <92797772+nishu-mns@users.noreply.github.com>
2021-10-19 23:15:49 +0530

    Add files via upload

---
+++ Project/scriptapi.js
@@ -0,0 +1,45 @@
+let weather = {
+    apiKey: "66c0a25f29581738dc32d4ee9737dcc2",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No city found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      document.querySelector(".city").innerText = "Weather in " + name;
+      
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = "Temperature: "+temp + "°C";
+      document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Wind speed: " + speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+      
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+  };
+  
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+  
+  
+  
+ 
